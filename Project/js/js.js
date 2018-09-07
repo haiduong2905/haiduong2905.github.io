e59cf43470d3ac6e3e399cd1835f920c80e6a3b6
Hai Duong <haiduong2905@gmail.com>
2018-09-07 10:15:38 +0700

    up

--- Project/js/js.js
+++ Project/js/js.js
@@ -87,10 +87,12 @@ function plusSlides() {
     var slides = document.getElementsByClassName("mySlides");
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
+
     }
     slideIndex++;
     if (slideIndex > slides.length) { slideIndex = 1 }
     slides[slideIndex - 1].style.display = "block";
+
 }
 
 function showSlides() {
@@ -102,7 +104,7 @@ function showSlides() {
     slideIndex++;
     if (slideIndex > slides.length) { slideIndex = 1 }
     slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 5000);
+    setTimeout(showSlides, 4000);
 }
 // Tab
 function openRoom(evt, roomName) {
